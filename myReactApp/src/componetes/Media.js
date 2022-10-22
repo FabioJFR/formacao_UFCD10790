98fc141051c42cf9832e01e67879aa7fee075ca4
FabioJFR <fabiojfrevez@gmail.com>
2022-10-22 17:45:43 +0100

    22/10/22/17:47

--- myReactApp/src/componetes/Media.js
+++ myReactApp/src/componetes/Media.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 function Media() {
 
+  //comando para gerar componente React : rfce
+  // comando para refrescar as branches : git fecth
+  //comando para ir buscar alterações : git pull
+  //comando para visualizr a branch : git branch
+
   let nrAlunos = 25;
   let nrPosts = 100;
   return (
@@ -14,8 +19,3 @@ function Media() {
 }
 
 export default Media
-
-//comando para gerar componente React : rfce
-// comando para refrescar as branches : git fecth
-//comando para ir buscar alterações : git pull
-//comando para visualizr a branch : git branch
